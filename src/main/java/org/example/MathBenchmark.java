f12bb1a8db134cb63e32a3527f2f9d7973ec01f2
Krzysztof Kowalski <--global>
2023-07-23 23:47:26 +0200

    update

--- src/main/java/org/example/MathBenchmark.java
+++ src/main/java/org/example/MathBenchmark.java
@@ -1,3 +1,22 @@
+/*
+ *
+ * This file is part of arche-quickstart.
+ *  Copyright (c) 2023.
+ * arche-quickstart is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * [Project Name] is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with arche-quickstart. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.example;
 
 import jdk.incubator.vector.FloatVector;
